b7201bdc33edbda52d5ae8bbe2dc5b41d577620c
Yajme <spartan.emjayrongavilla@gmail.com>
2026-06-19 00:30:45 +0800

    chore: update controllers index file to include exports for all controllers

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,1 +1,7 @@
 // Controllers index - exports for all controllers
+//
+
+export * from './apiController';
+export * from './authController';
+export * from './mockController';
+export * from './userController';

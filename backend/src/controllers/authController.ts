2873a15ab6a273d16fd0e9d54f3fb73ab43a2fb4
Yajme <spartan.emjayrongavilla@gmail.com>
2026-06-19 00:29:58 +0800

    feat: implement signup functionality and enhance login validation

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,8 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
 import { RegisterBody, LoginBody } from '../types/request.js';
-import { loginUser, registerUser } from '../services/userService.js';
+import { loginUser, registerUser, signupUser } from '../services/userService.js';
 import { HttpStatus } from '../utils/httpStatus.js';
+import { validateFields } from '#src/utils/validateFields';
+import { signupSchema } from '#src/schema/userSchema';
 
+export const signup = async (req:Request, res: Response, next:NextFunction):Promise<void> =>{
+  try {
+    const {
+      email,
+      password,
+      first_name,
+      last_name
+    } = req.body;
+    const signupBody = {email,password,first_name,last_name}
+    validateFields(signupSchema,signupBody);
+
+    const user = await signupUser(signupBody
+    )
+
+    res.locals.message = "User Sign up successful";
+    res.locals.data = {user}
+    next();
+  } catch (error) {
+    next(error)
+  }
+}
 export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const { username, email, password } = req.body as RegisterBody;
@@ -20,8 +43,11 @@ export const register = async (req: Request, res: Response, next: NextFunction):
 
 export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
+    if(!req.body?.email || !req.body?.password) {
+      throw new Error('Email and password are required');
+    }
     const { email, password } = req.body as LoginBody;
-
+    
     const token = await loginUser(email, password);
 
     res.locals.data = { token };
--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,6 +1,17 @@
-import { connection } from '../config/database';
-import bcrypt from 'bcryptjs';
-import { HttpStatus, UserError, AuthError, NotFoundError, ForbiddenError, signToken } from '../utils/index';
+import { connection } from "../config/database";
+import bcrypt from "bcryptjs";
+import {
+  HttpStatus,
+  UserError,
+  AuthError,
+  NotFoundError,
+  ForbiddenError,
+  signToken,
+} from "../utils/index";
+import { supabase } from "#src/config/supabase";
+import { SignupDto } from "../schema";
+
+// This can be deprecated soon
 
 interface IUser {
   id?: string;
@@ -13,7 +24,10 @@ interface IUser {
 
 const SALT_ROUNDS = 12;
 
-const verifyPassword = async (rawPass: string, storedHash: string): Promise<boolean> => {
+const verifyPassword = async (
+  rawPass: string,
+  storedHash: string,
+): Promise<boolean> => {
   return await bcrypt.compare(rawPass, storedHash);
 };
 
@@ -21,9 +35,13 @@ const hashPassword = async (rawPass: string): Promise<string> => {
   return await bcrypt.hash(rawPass, SALT_ROUNDS);
 };
 
-const isUserExisting = async (username: string, email: string): Promise<boolean> => {
+const isUserExisting = async (
+  username: string,
+  email: string,
+): Promise<boolean> => {
   try {
-    const query = "SELECT username,email FROM users WHERE username = $1 OR email = $2";
+    const query =
+      "SELECT username,email FROM users WHERE username = $1 OR email = $2";
     const response = await connection.query(query, [username, email]);
     return (response.rowCount ?? 0) > 0;
   } catch (error) {
@@ -34,37 +52,70 @@ const isUserExisting = async (username: string, email: string): Promise<boolean>
 export const registerUser = async (user: IUser = {}): Promise<void> => {
   try {
     const { username, email, password } = user;
-    if (!username || !email || !password) throw new UserError('All fields are required', HttpStatus.BAD_REQUEST);
-    
+    if (!username || !email || !password)
+      throw new UserError("All fields are required", HttpStatus.BAD_REQUEST);
+
     const userExists = await isUserExisting(username, email);
-    if (userExists) throw new ForbiddenError("This username or email already exist");
+    if (userExists)
+      throw new ForbiddenError("This username or email already exist");
 
     const hashedPassword = await hashPassword(password);
 
-    const query = "INSERT INTO users (username,email,password_hash) VALUES ($1,$2,$3) RETURNING id;";
-    const result = await connection.query(query, [username, email, hashedPassword]);
+    const query =
+      "INSERT INTO users (username,email,password_hash) VALUES ($1,$2,$3) RETURNING id;";
+    const result = await connection.query(query, [
+      username,
+      email,
+      hashedPassword,
+    ]);
 
-    if ((result.rowCount ?? 0) < 1) throw new Error('There is an error with registering user');
+    if ((result.rowCount ?? 0) < 1)
+      throw new Error("There is an error with registering user");
   } catch (error) {
     throw error;
   }
 };
 
-export const loginUser = async (email: string, password: string): Promise<string> => {
+export const loginUser = async (
+  email: string,
+  password: string,
+): Promise<Record<string,any>> => {
   try {
-    if (!email || !password) throw new UserError("Fields must be filled", HttpStatus.BAD_REQUEST);
-
-    const query = "SELECT password_hash,id,username FROM users WHERE deleted_at IS NULL AND email = $1";
-    const response = await connection.query(query, [email]);
-
-    if ((response.rowCount ?? 0) < 1) throw new NotFoundError("User not found");
-    const user = response.rows[0];
-    const valid = await verifyPassword(password, user.password_hash);
-    if (!valid) throw new AuthError("Invalid Password");
-
-    const token = signToken({ id: user.id, username: user.username });
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    const user = data.user;
+    if (error) {
+      throw error;
+    }
+    const token = data.session;
     return token;
   } catch (error) {
     throw error;
   }
+};
+
+export const signupUser = async ({
+  email,
+  password,
+  first_name,
+  last_name,
+}: SignupDto) => {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        first_name,
+        last_name,
+      },
+    },
+  });
+
+  if (error) {
+    throw error;
+  }
+
+  return data.user;
 };

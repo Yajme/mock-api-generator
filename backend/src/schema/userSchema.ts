d79e2d497aabfb027675aaffe8f5392a8322364e
Yajme <spartan.emjayrongavilla@gmail.com>
2026-06-19 00:30:52 +0800

    feat: add signup schema for user registration validation

--- backend/src/schema/userSchema.ts
+++ backend/src/schema/userSchema.ts
@@ -1,6 +1,11 @@
 // src/schemas/userSchema.ts
 import { z } from "zod";
-
+export const signupSchema = z.object({
+  email: z.string().email("Email is required"),
+  password: z.string().min(8,"Passwords should be minimum of 8 characters"),
+  first_name: z.string().min(1, "First should not be empty"),
+  last_name: z.string().min(1, "Last name should not be empty")
+})
 export const updateProfileSchema = z
   .object({
     username: z.string().min(3).max(30).optional(),
@@ -14,5 +19,6 @@ export const deleteAccountSchema = z.object({
   password: z.string().min(1),
 });
 
+export type SignupDto = z.infer<typeof signupSchema>;
 export type UpdateProfileBody = z.infer<typeof updateProfileSchema>;
 export type DeleteAccountBody = z.infer<typeof deleteAccountSchema>;
